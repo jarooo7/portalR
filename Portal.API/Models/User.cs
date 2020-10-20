e6d7f819abb4f2440384b4c288d722491d561fb1
@jarek564as <jarek564as@gamil.com>
2020-10-20 23:07:30 +0200

    update User model API, added class model Photo

--- Portal.API/Models/User.cs
+++ Portal.API/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace Portal.API.Models
 {
     public class User
@@ -6,5 +9,36 @@ namespace Portal.API.Models
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastAction { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+
+        public string Growth { get; set; }
+        public string EyeColor { get; set; }
+        public string HairColor { get; set; }
+        public string MartialStatus { get; set; }
+        public string Edukation { get; set; }
+        public string Profession { get; set; }
+        public string Children { get; set; }
+        public string Languages { get; set; }
+
+        public string Description { get; set; }
+        public string Personality { get; set; }
+        public string LookingFor { get; set; }
+
+
+        public string Interests { get; set; }
+        public string FreeTime { get; set; }
+
+        public string ILike { get; set; }
+        public string IDontLike { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+    
     }
 }
