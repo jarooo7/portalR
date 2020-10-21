69c499d4b37ec3871626419c42a1d2fe33cfc7a7
@jarek564as <jarek564as@gamil.com>
2020-10-21 14:28:51 +0200

    Add GenericRepository and UserRepository

---
+++ Portal.API/Data/UserRepository.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Portal.API.Models;
+
+namespace Portal.API.Data
+{
+    public class UserRepository : GenericRepository, IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            User user = await _context.Users.Include(p=>p.Photos).FirstOrDefaultAsync(u=>u.Id == id);
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users =  await _context.Users.Include(p=>p.Photos).ToListAsync();
+            return users;
+        }
+    }
+}
