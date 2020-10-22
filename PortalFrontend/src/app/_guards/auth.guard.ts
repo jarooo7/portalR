fc1077f622dcacf4df115d18d20382220db02648
@jarek564as <jarek564as@gamil.com>
2020-10-22 23:34:27 +0200

    Routing and Auth Guard Front

---
+++ PortalFrontend/src/app/_guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AlertifyService } from '../_serwises/alertify/alertify.service';
+import { AuthService } from '../_serwises/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+ 
+  constructor(private auth: AuthService, private router: Router, private alertify: AlertifyService) {
+
+  }
+
+
+  canActivate(): boolean {
+
+    if(this.auth.loggedIn()){
+    return true;}
+    this.alertify.error("Nie masz uprawnień");
+    this.router.navigate(['/home']);
+    return false;
+  }
+
+}
--- PortalFrontend/src/app/app.module.ts
+++ PortalFrontend/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { LikesComponent } from './likes/likes.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 export function getToken() {
   return localStorage.getItem('token');
@@ -49,7 +50,8 @@ export function getToken() {
   providers: [
     AuthService,
     AlertifyService,
-    UserService
+    UserService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
